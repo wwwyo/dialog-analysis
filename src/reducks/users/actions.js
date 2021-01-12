7bce171a16610634844ac57032f53cb650424cc3
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-01-12 12:52:02 +0900

    [Add] users アクションズ

---
+++ src/reducks/users/actions.js
@@ -0,0 +1,24 @@
+export const SIGN_IN = "SIGN_IN";
+export const signInAction = (userState) => {
+  return {
+    type: "SIGN_IN",
+    payload: {
+      isSignedIn: true,
+      nickname: userState.nickname,
+      uid: userState.uid,
+    }
+  }
+};
+
+export const SIGN_OUT = "SIGN_OUT";
+export const signOutAction = () => {
+  return {
+    type: "SIGN_OUT",
+    payload: {
+      isSignedIn: false,
+      nickname: "",
+      uid: "",
+    }
+  }
+};
+
