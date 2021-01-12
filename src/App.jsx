494486fdfb579cd3bbdfdc30b0a0ab75b6bfd3b2
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-01-12 18:01:49 +0900

    [Add] ヘッダー読み込み

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Router from './Router';
 import './assets/reset.css';
 import './assets/style.css';
+import {Header} from './components/header/index';
 
 const App = () => {
   return (
-    <main>
-      <Router />
-    </main>
+    <>
+      <Header />
+      <main>
+        <Router />
+      </main>
+    </>
   )
 }
 
