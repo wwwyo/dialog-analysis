e23e58ac5fe2512bd456e86ac7c0088bba0af208
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-01-12 15:47:17 +0900

    [Add] テンプレre-export

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,3 +1,5 @@
+export {default as Auth} from './Auth'
 export {default as Home} from './Home';
+export {default as Reset} from './Reset';
 export {default as SignUp} from './SignUp';
 export {default as SignIn} from './SignIn';
