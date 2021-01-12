e825f9e077a7b75f38571e5c59e1609df93a52ea
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-01-12 12:45:11 +0900

    [Add] router

---
+++ src/Router.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Route, Switch} from 'react-router';
+import {SignUp, Home, SignIn} from './templates';
+
+const Router = () => {
+  return (
+    <Switch>
+      <Route exact path={"/signup"} component={SignUp} />
+      <Route exact path={"/signin"} component={SignIn} />
+      <Route exact path={"(/)?"} component={Home} />
+    </Switch>
+  )
+}
+
+export default Router;
