c4aa2672105f8f362e9aa5a12eecced2dcb96fc0
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-01-12 15:46:13 +0900

    [Add] selectors ログイン状況取得

--- src/reducks/users/selectors.js
+++ src/reducks/users/selectors.js
@@ -2,6 +2,10 @@ import {createSelector} from 'reselect';
 
 const usersSelector = (state) => state.users;
 
+export const getIsSignedIn = createSelector(
+  [usersSelector],
+  state => state.isSignedIn
+)
 export const getUserId = createSelector(
   [usersSelector],
   state => state.uid
