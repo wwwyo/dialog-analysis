49be259d363c83788e38e57f8a1dd0a31aa4e96e
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-01-12 12:51:17 +0900

    [Add] src/index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {Provider} from 'react-redux';
+import createStore from './reducks/store/store';
+import {ConnectedRouter} from 'connected-react-router';
+import * as History from 'history';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 
+const history = History.createBrowserHistory();
+export const store = createStore(history);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ConnectedRouter history={history} >
+      <App />
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
